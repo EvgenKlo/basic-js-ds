09e4ab24654a5127a69427cc4a05caafcf5466ae
EvgenKlo <klokovevgen@gmail.com>
2023-04-10 15:02:47 +0500

    feat: done queue

--- src/queue.js
+++ src/queue.js
@@ -13,20 +13,160 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+let queue = [];
+
 class Queue {
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    queue.pop();
+    let cloneQueue = [];
+    cloneQueue = cloneQueue.concat(queue);
+    let result = {};
+    if (cloneQueue){
+      result.value = cloneQueue[cloneQueue.length - 1];
+      result.next = null;
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    if (cloneQueue.length > 0){
+      result.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next = {
+        value: cloneQueue[cloneQueue.length - 1],
+        next: null
+      }
+      cloneQueue.pop();
+    }
+    return result;
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    queue.unshift(value);
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    return queue.pop()
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
